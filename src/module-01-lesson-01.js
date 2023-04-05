2ad24cde741d9b65ade91a5f285d72054907aa09
Verlong <bonko23@gmail.com>
2023-04-05 15:56:48 +0300

    1.3 2.1 2.2

--- src/module-01-lesson-01.js
+++ src/module-01-lesson-01.js
@@ -33,3 +33,60 @@
 // console.log(calculateBMI('88,3', '1.75')); // 28.7
 
 //////////////////////////////////////////////////////////////////////////////////////////
+
+// 3. Перше століття охоплює період з 1-го по 100-й рік включно, друге - з 101-го по 200-й рік включно і т.д. За заданим роком повернути століття, в якому він знаходиться.
+
+// function centuryFromYear(year) {
+//   let century = year / 100;
+//   //   console.log(century);
+//   if (year % 100 > 0) {
+//     century++;
+//   }
+//   return Math.floor(century);
+// }
+// console.log(centuryFromYear(1705)); // 18
+// console.log(centuryFromYear(1900)); // 19
+// console.log(centuryFromYear(1601)); // 17
+// console.log(centuryFromYear(2000)); // 20
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Блок 2 - Робота з рядками, методи рядків
+// 1. Перевірь чи param є рядком. Результатом виконання функції має бути true або false
+
+// function isString(param) {
+//   let isString = typeof param;
+//   if (isString === `string`) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+//   isString = isString === `string` ? true : false;
+//   return typeof param === `string`;
+// }
+
+// console.log(isString('hello world'));
+// console.log(isString('undefined'));
+// console.log(isString([1, 2, 4, 0]));
+// console.log(isString({ course: 'GoIT' }));
+// console.log(isString(+'5'));
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// 2. Напишіть функцію для перетворення імені в ініціали. Ви отримаєтеім'я з прізвищем розділені пробілом. В результаті це має виглядати так:
+// `Sam Harris => S.H`
+// `Patrick Feeney => P.F`
+
+// function abbrevName(name) {
+//   console.log(name.split(' '));
+//   const fullName = name.split(' ');
+//   const firstName = fullName[0];
+//   const lastName = fullName[1];
+//   console.log(firstName[0]);
+//   console.log(lastName[0]);
+//   return `${firstName[0]}.${lastName[0]}.`;
+// }
+
+// console.log(abbrevName('Sam Harris'));
+// console.log(abbrevName('Patrick Feeney'));
+////////////////////////////////////////////////////////////////////////////////////////////////
