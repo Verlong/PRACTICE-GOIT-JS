44b5b1de0fac947bba95e1f38c3590942e7677f6
Verlong <bonko23@gmail.com>
2023-04-17 14:13:18 +0300

    м 4/1

---
+++ src/module-04-lesson-01.js
@@ -0,0 +1,12 @@
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
