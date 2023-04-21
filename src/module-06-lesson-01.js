09ef0a2298caf182ba23a4928dcbe75cc56f0b4b
Verlong <bonko23@gmail.com>
2023-04-21 22:59:57 +0300

    dom

--- src/module-06-lesson-01.js
+++ src/module-06-lesson-01.js
@@ -85,3 +85,25 @@
 // </li>`;
 // ulEl.insertAdjacentHTML('beforeend', markup);
 // // очисти список
+// const list = document.querySelector('.list');
+// list.remove();
+// ========================Пошук елементів======================================================================================================
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.menu');
+// listWithClass.style.backgroundColor = 'yellow';
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll('li');
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll('.menu-item');
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector('.menu-item');
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+// ========================/Пошук елементів======================================================================================================
